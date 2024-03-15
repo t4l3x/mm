66c8450db3bb86b4ec26392b12fc5521fbf1ee1c
Taleh <t4lex999@gmail.com>
2024-03-15 15:41:40 +0200

    fixes

--- src/Services/MoySkladDataService.php
+++ src/Services/MoySkladDataService.php
@@ -117,7 +117,7 @@ class MoySkladDataService
                 ->customerorder()
                 ->create($orderData);
         } catch (\Exception $e) {
-            throw new \Exception('Failed to create customer order: ' . ' order id ' .$orderData['name'] ?? '@'.' '. $e->getMessage(), $e->getCode(), $e);
+            throw new \Exception('Failed to create customer order: ' . $e->getMessage() . ' order id ' .$orderData['name'] ?? '@', $e->getCode(), $e);
         }
     }
 
