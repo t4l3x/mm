824b3f77f3d23518cf04b91016c330a9d4545829
Taleh <t4lex999@gmail.com>
2024-03-15 21:18:49 +0200

    fixess

--- src/Services/OrderProductService.php
+++ src/Services/OrderProductService.php
@@ -115,7 +115,6 @@ class OrderProductService
 
         $productType = $product->getComponent() ? 'bundle' : 'product';
         $product = $product->getMoysklad();
-        echo "https://api.moysklad.ru/api/remap/1.2/entity/" . $productType . '/' . $product . "\n";
         return [
             'quantity' => floatval($orderProduct->getQuantity()) ?? 1,
             'reserve' => floatval($orderProduct->getQuantity()) ?? 1,
